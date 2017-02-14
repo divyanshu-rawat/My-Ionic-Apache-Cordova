9195f7d655a68a575f0822ae9a68260ec8ec8912
divyanshu <divyanshu.r46956@gmail.com>
2017-02-15 00:46:41 +0530

    added server-side rest-api code !

---
+++ models.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+var _ = require('underscore');
+
+module.exports = function(wagner) {
+  mongoose.connect('mongodb://divyanshu:12345@ds017584.mlab.com:17584/mean_stk');
+ 
+  // mongodb://divyanshu:12345@ds017584.mlab.com:17584/mean_stk
+ // mongodb://localhost:27017/angular_client
+
+  wagner.factory('db', function() {
+    return mongoose;
+  });
+
+  var Category =
+    mongoose.model('Category', require('./category'), 'categories');
+  var User =
+    mongoose.model('User', require('./user'), 'users');
+
+  var models = {
+    Category: Category,
+    User: User
+  };
+
+  // To ensure DRY-ness, register factories in a loop
+  _.each(models, function(value, key) {
+    wagner.factory(key, function() {
+      return value;
+    });
+  });
+
+  wagner.factory('Product', require('./product'));
+
+  return models;
+};
